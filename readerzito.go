a25252b6bd0912665258aeb7891d55b06d97e732
Andrews Medina <andrewsmedina@gmail.com>
2013-03-14 02:23:43 -0300

    parser rss 2.0.
    Related to #1.

--- readerzito.go
+++ readerzito.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"labix.org/v2/mgo"
 	"time"
 )
@@ -29,4 +30,37 @@ type feeditem struct {
 func subscribe(name, url string) error {
 	f := feed{Name: name, Url: url}
 	return collection.Insert(f)
+}
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+	Item []Item `xml:"item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Link        string `xml:"link"`
+}
+
+func parse(data string) ([]feeditem, error) {
+	var r RSS
+	var items []feeditem
+	err := xml.Unmarshal([]byte(data), &r)
+	if err != nil {
+		return items, err
+	}
+	for _, item := range r.Channel.Item {
+		i := feeditem{
+			title:   item.Title,
+			content: item.Description,
+			url:     item.Link,
+		}
+		items = append(items, i)
+	}
+	return items, nil
 }
